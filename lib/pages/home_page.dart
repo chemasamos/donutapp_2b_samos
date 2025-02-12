7ac5ae233a5033bff0e98110d75205b5dbf6ecbe
Jose Samos <chemasassss@gmail.com>
2025-02-12 11:38:03 -0600

    TabBar Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:donutapp_2b_samos/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,18 +9,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Lista de tabs
+  List<Widget> myTabs = [
+    //Ct Tab
+    MyTab(iconPath: 'lib/icons/corrido.png'),
+    //BurgerTab
+    MyTab(iconPath: 'lib/icons/burger.png'),
+    //SmoothieTab
+    MyTab(iconPath: 'lib/icons/smoothie.png'),
+    //PancakeTab
+    MyTab(iconPath: 'lib/icons/pancake.png'),
+    //DonutTab
+    MyTab(iconPath: 'lib/icons/donut.png'),
+    ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        // icono izquierdo
-        leading: Icon(
-          Icons.menu,
-          color: Colors.pinkAccent[800],
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          // icono izquierdo
+          leading: Icon(
+            Icons.menu,
+            color: Colors.pinkAccent[800],
+          ),
+          // Icono derecho
+          actions: const[Padding(
+            padding: EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )]
         ),
-        // Icono derecho
-        actions: [Icon(Icons.person)]
+        body: Column(
+          children: [
+          // texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: const[
+                  Text("I want to ", style: TextStyle(fontSize: 32 )),
+                  Text("Tragar", style: TextStyle(
+                  // Letra tamaño
+                    fontSize: 32,
+                  // Negritas
+                  fontWeight: FontWeight.bold,
+                  // Subrayado
+                  decoration: TextDecoration.underline))
+                  ],
+              ),
+            ),
+          // tabBar
+          TabBar(tabs: myTabs)
+          //tabBarview
+      
+          //carrito
+          ],
+        )
       ),
     );
   }
